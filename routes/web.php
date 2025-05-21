95154722e8f2ad2146f70004eb84ccad2e7a63fa
kimhao97 <nguyenkimhaodn@gmail.com>
2025-05-21 11:00:10 +0700

    feat: added course, classroom, course UI

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\web\HomeController;
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\admin\CourseController;
+use App\Http\Controllers\admin\ClassroomController;
+use App\Http\Controllers\admin\CommentController;
+
 Route::get('/', [HomeController::class, 'index'])->name('web.home');
 
 Route::get('/admin', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::get('/admin/course', [CourseController::class, 'index'])->name('admin.course');
+Route::get('/admin/classroom', [ClassroomController::class, 'index'])->name('admin.classroom');
+Route::get('/admin/comment', [CommentController::class, 'index'])->name('admin.comment');
