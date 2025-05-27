4c03ac25d17cee388a09f1f6e1d6de24f065765d
kimhao97 <nguyenkimhaodn@gmail.com>
2025-05-27 10:48:46 +0700

    feat: create Student comment migration and model

---
+++ database/migrations/2025_05_26_020522_create_student_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_name');
+            $table->string('content');
+            $table->string('avatar_url')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_comments');
+    }
+};
